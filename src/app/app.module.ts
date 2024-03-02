f57293cab11b8441db168639a5d0bc1bf5b9f9ba
Kenneth Barrett <hamptonalumkb@proton.me>
2024-03-01 20:31:54 -0500

    Create new post and all post components and add routes
    	modified:   src/app/app-routing.module.ts
    	modified:   src/app/app.module.ts
    	new file:   src/app/posts/all-post/all-post.component.css
    	new file:   src/app/posts/all-post/all-post.component.html
    	new file:   src/app/posts/all-post/all-post.component.spec.ts
    	new file:   src/app/posts/all-post/all-post.component.ts
    	new file:   src/app/posts/new-post/new-post.component.css
    	new file:   src/app/posts/new-post/new-post.component.html
    	new file:   src/app/posts/new-post/new-post.component.spec.ts
    	new file:   src/app/posts/new-post/new-post.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { AllPostComponent } from './posts/all-post/all-post.component';
+import { NewPostComponent } from './posts/new-post/new-post.component';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import { CategoriesComponent } from './categories/categories.component';
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    AllPostComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
